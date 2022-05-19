d2552d77df167f276f94457a07f9beda16c774a1
Jonathan McLachlan <jonathan.mclachlan@hotmail.com>
2022-05-19 11:45:09 +0200

    cleaning thursday am

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: 'London',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+5.times do
+  Flat.create(
+  name: Faker::FunnyName.two_word_name,
+  address:Faker::Address.city,
+  description: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+  price_per_night: [75,15,25,26,28,56,78].sample,
+  number_of_guests: [1,2,3,4,5,6].sample
+  )
+end
+puts "done with seeding"
